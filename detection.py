4108a161618fd7b833f9b57a3b070c436190b962
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-14 21:36:30 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -2,23 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread("comp.jpg", cv2.IMREAD_GRAYSCALE)
-lap = cv2.Laplacian(img, cv2.CV_64F, ksize=3)
-lap = np.uint8(np.absolute(lap))
-sobelX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
-sobelY = cv2.Sobel(img, cv2.CV_64F, 0, 1)
-edges = cv2.Canny(img,100,200)
+image = cv2.imread("comp.jpg", cv2.IMREAD_GRAYSCALE)
+laplacian = cv2.Laplacian(image, cv2.CV_64F, ksize=3)
+laplacian = np.uint8(np.absolute(laplacian))
+sobelX = cv2.Sobel(image, cv2.CV_64F, 1, 0)
+sobelY = cv2.Sobel(image, cv2.CV_64F, 0, 1)
+canny = cv2.Canny(image,100,200)
 
 sobelX = np.uint8(np.absolute(sobelX))
 sobelY = np.uint8(np.absolute(sobelY))
 
 sobelCombined = cv2.bitwise_or(sobelX, sobelY)
 
-titles = ['image', 'Laplacian', 'sobelX', 'sobelY', 'sobelCombined', 'Canny']
-images = [img, lap, sobelX, sobelY, sobelCombined, edges]
+names = ['image', 'Laplacian', 'sobelX', 'sobelY', 'sobelCombined', 'Canny']
+imgs = [image, laplacian, sobelX, sobelY, sobelCombined, canny]
 for i in range(6):
-    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
+    plt.subplot(2, 3, i+1), plt.imshow(imgs[i], 'gray')
+    plt.title(names[i])
     plt.xticks([]),plt.yticks([])
 
 plt.show()
